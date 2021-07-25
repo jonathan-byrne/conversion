4fc0026caa8781be412abfef2061970cee413161
jonathan-byrne <jonnoponno@gmail.com>
2021-07-25 09:32:25 +0200

    Added unit tests and fixed bug with missing extends SpringBootServletInitializer command

--- src/main/java/com/ayo/conversion/ConversionApplication.java
+++ src/main/java/com/ayo/conversion/ConversionApplication.java
@@ -2,9 +2,10 @@ package com.ayo.conversion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ConversionApplication {
+public class ConversionApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConversionApplication.class, args);
--- src/test/java/com/ayo/conversion/InchesAndCentimetersConverterTests.java
+++ src/test/java/com/ayo/conversion/InchesAndCentimetersConverterTests.java
@@ -6,27 +6,38 @@ import com.ayo.conversion.converter.InchesToCentimetersConverter;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
-public class ConverterTests {
+public class InchesAndCentimetersConverterTests {
 
+    /**
+     * Testing to check that the InchesToCentimetersConverter is idempotent
+     *
+     * It would be a design flaw to hardcode the calculation in this unit test,
+     * if the calculation is changed in future a unit test with a hardcoded calculation
+     * would fail.
+     *
+     * This way we can check the consistency of the converter regardless of
+     * any changes to it's calculation
+     */
     @Test
     public void convertInchesToCentimeters() {
         Converter<Double> inchesToCentimetersConverter = new InchesToCentimetersConverter();
         Double centimetersSizeOne = inchesToCentimetersConverter.convert(1.0);
         Double centimetersSizeTwo = inchesToCentimetersConverter.convert(1.0);
 
-        //Checking this assert, not performing manual calculations here as the implementations could
-        //change in the converter classes themselves
         Assert.assertEquals(centimetersSizeOne, centimetersSizeTwo);
     }
 
+    /**
+     * This tests whether the CentimetersToInchesConverter is idempotent
+     *
+     * see convertInchesToCentimeters() above for information on the design decisions for this unit test
+     */
     @Test
     public void convertCentimetersToInches() {
         Converter<Double> centimetersToInchesConverter = new CentimetersToInchesConverter();
         Double inchesSizeOne = centimetersToInchesConverter.convert(1.0);
         Double inchesSizeTwo = centimetersToInchesConverter.convert(1.0);
 
-        //Checking this assert, not performing manual calculations here as the implementations could
-        //change in the converter classes themselves
         Assert.assertEquals(inchesSizeOne, inchesSizeTwo);
     }
 }
